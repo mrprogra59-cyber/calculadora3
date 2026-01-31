c4c35e88d0083619313765952925aa1ee32ef37f
PC-40 <PC-40@cpu40>
2026-01-31 10:58:17 -0600

    codigoPrincipal

---
+++ src/calculadora3b/Calculadora3b.java
@@ -0,0 +1,26 @@
+
+package calculadora3b;
+
+import java.util.Scanner;
+
+
+public class Calculadora3b {
+
+    
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer número:");
+        int a = sc.nextInt();
+
+        System.out.println("Ingrese el segundo número:");
+        int b = sc.nextInt();
+
+        //Suma suma = new Suma();
+        //Resta resta = new Resta();
+        
+        //System.out.println("Suma: " + suma.calcular(a, b));
+        //System.out.println("Resta: " + resta.calcular(a, b));
+    }
+    
+}
